e5df850eab15cf5cb1401d5c24848adbeeb0f4e1
Nick Sharp <nick.sharp.87@gmail.com>
2021-06-16 17:03:47 +0100

    updating sequelize cli to 6

--- app/orm/migrations/000001.create-peripherals-table.ts
+++ app/orm/migrations/000001.create-peripherals-table.ts
@@ -1,37 +1,38 @@
 const TABLE_NAME = 'peripherals';
 
-export function up(queryInterface, Sequelize) {
-  return queryInterface.createTable(TABLE_NAME, {
-    id: {
-      type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4,
-      primaryKey: true,
-    },
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    type: {
-      type: Sequelize.ENUM('heater'),
-      allowNull: false,
-    },
-    communicationType: {
-      type: Sequelize.ENUM('gpio'),
-      allowNull: false,
-    },
-    gpio: {
-      type: Sequelize.INTEGER,
-      allowNull: true,
-    },
-    createdAt: {
-      type: Sequelize.DATE,
-    },
-    updatedAt: {
-      type: Sequelize.DATE,
-    },
-  });
-}
-
-export function down(queryInterface, Sequelize) {
-  return queryInterface.dropTable(TABLE_NAME);
-}
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable(TABLE_NAME, {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      type: {
+        type: Sequelize.ENUM('heater'),
+        allowNull: false,
+      },
+      communicationType: {
+        type: Sequelize.ENUM('gpio'),
+        allowNull: false,
+      },
+      gpio: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable(TABLE_NAME);
+  },
+};
