55f678018c4d61eca359a98cecb415a0937e6f0f
Nick Sharp <nick.sharp.87@gmail.com>
2022-06-24 14:11:47 +0100

    test: adding telemetry and translations tests

--- apps/homebrew/src/app/translations/service.spec.ts
+++ apps/homebrew/src/app/translations/service.spec.ts
@@ -1,3 +1,65 @@
+import { Test } from '@nestjs/testing';
+import { IActors, ILogics, ISensors } from '../../lib/constants';
+import { TranslationsService } from './service';
+
 describe('translations-service', () => {
-  it('a', () => {});
+  let service: TranslationsService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [TranslationsService],
+    })
+      .useMocker((token) => {
+        if (token === IActors) {
+          return [
+            {
+              name: 'testing-actor',
+              localizations: {
+                en: {},
+              },
+            },
+          ];
+        }
+
+        if (token === ISensors) {
+          return [
+            {
+              name: 'testing-sensor',
+              localizations: {
+                en: {},
+              },
+            },
+          ];
+        }
+
+        if (token === ILogics) {
+          return [
+            {
+              name: 'testing-logic',
+              localizations: {
+                en: {},
+              },
+            },
+          ];
+        }
+      })
+      .compile();
+
+    service = moduleRef.get(TranslationsService);
+  });
+
+  describe('generateTranslations', () => {
+    it('should generate the translations', async () => {
+      const { translations, namespaces, locales } =
+        await service.generateTranslations();
+
+      expect(locales).toStrictEqual(['en']);
+      expect(namespaces).toEqual([
+        'testing-sensor',
+        'testing-actor',
+        'testing-logic',
+        'common',
+      ]);
+    });
+  });
 });
