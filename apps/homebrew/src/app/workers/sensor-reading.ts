46c55687b6284ca003ba2414622f7ec582480857
Nick Sharp <nick.sharp.87@gmail.com>
2022-07-04 14:52:34 +0100

    test: updating tests

--- apps/homebrew/src/app/workers/sensor-reading.ts
+++ apps/homebrew/src/app/workers/sensor-reading.ts
@@ -3,19 +3,18 @@ import { OnEvent } from '@nestjs/event-emitter';
 
 import { TelemetryService } from '../../lib/services/telemetry/service';
 import { NewSensorReading } from '../../models/events/new-sensor-reading';
-import { SocketGateway } from '../../socket-gateway/gateway';
 
 @Injectable()
 export class SensorReadingWorkerService {
   constructor(
-    private gateway: SocketGateway,
+    // private gateway: SocketGateway,
     private telemetryService: TelemetryService,
   ) {}
 
-  @OnEvent(NewSensorReading.Channel)
-  async sendEventToUi(payload: NewSensorReading) {
-    this.gateway.sendMessage(NewSensorReading.Channel, payload);
-  }
+  // @OnEvent(NewSensorReading.Channel)
+  // async sendEventToUi(payload: NewSensorReading) {
+  //   this.gateway.sendMessage(NewSensorReading.Channel, payload);
+  // }
 
   @OnEvent(NewSensorReading.Channel)
   async saveSensorTelemetry(payload: NewSensorReading) {
--- apps/homebrew/test/app/workers/sensor-reading.e2e-spec.ts
+++ apps/homebrew/test/app/workers/sensor-reading.e2e-spec.ts
@@ -1,1 +1,57 @@
-describe('sensor-reading (e2e)');
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { Test } from '@nestjs/testing';
+import { CachingModule } from '@ota-internal/caching';
+import { LockingModule } from '@ota-internal/locking';
+import { SensorReadingWorkerService } from '../../../src/app/workers/sensor-reading';
+import { DatabaseModule } from '../../../src/database/module';
+import { ServicesModule } from '../../../src/lib/services/module';
+import { cleanup, IRepositories } from '../../utils/cleanup';
+
+describe('sensor-reading (e2e)', () => {
+  let service: SensorReadingWorkerService;
+  let repositories: IRepositories;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [SensorReadingWorkerService],
+      imports: [
+        ServicesModule,
+        EventEmitterModule.forRoot(),
+        DatabaseModule,
+        CachingModule,
+        LockingModule,
+      ],
+    }).compile();
+
+    service = moduleRef.get(SensorReadingWorkerService);
+    repositories = await cleanup(moduleRef);
+  });
+
+  it('saveSensorTelemetry', async () => {
+    const { id: device_id } = await repositories.devices.create({
+      name: 'test',
+      type_id: 'local-device',
+    });
+
+    const { id: sensor_id } = await repositories.sensors.create({
+      name: 'sensor_1',
+      device_id,
+      type_id: 'one-wire-sensor',
+      config: {},
+    });
+
+    await service.saveSensorTelemetry({
+      sensor_id,
+      reading: 10,
+    });
+
+    const refetchedTelemetry = await repositories.telemetry.findAll({
+      where: {
+        sensor_id,
+      },
+    });
+
+    expect(refetchedTelemetry).toHaveLength(1);
+    expect(refetchedTelemetry[0].reading).toBe(10);
+  });
+});
