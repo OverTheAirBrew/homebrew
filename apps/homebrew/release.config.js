2548482e8a7d997f6cf3eb29e6e4fbcd62b4a9e9
Nick Sharp <nick.sharp.87@gmail.com>
2022-07-06 11:24:42 +0100

    feat: adding docker build instead of npm

--- apps/homebrew/release.config.js
+++ apps/homebrew/release.config.js
@@ -11,7 +11,17 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
+    [
+      '@overtheairbrew/semantic-release-docker-buildx',
+      {
+        name: 'overtheairbrew/homebrew',
+        buildArgs: {
+          APP: 'homebrew',
+        },
+        platforms: ['linux/arm/v7'],
+        dockerFile: '../../Dockerfile',
+      },
+    ],
     '@semantic-release/github',
     '@semantic-release/git',
   ],
