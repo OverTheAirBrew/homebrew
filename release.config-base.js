72b3df5c24e618b4d774792f9c89408b4607069e
Nick Sharp <nick.sharp.87@gmail.com>
2022-07-17 11:22:32 +0100

    chore: debugging branches

--- release.config-base.js
+++ release.config-base.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 function generateReleaseConfig(appName) {
   const BRANCH = process.env.BRANCH;
+  const GITHUB_REF = process.env.GITHUB_REF;
 
   let DOCKER_TAGS = ['{{version}}'];
 
@@ -10,6 +11,15 @@ function generateReleaseConfig(appName) {
   } else if (BRANCH === 'next') {
     DOCKER_TAGS.push('next');
   }
+
+  console.log(
+    'BRANCH',
+    BRANCH,
+    'GITHUB_REF',
+    GITHUB_REF,
+    'DOCKER_TAGS',
+    DOCKER_TAGS,
+  );
 
   const dockerFileLocation = path.join(process.cwd(), '..', '..');
 
